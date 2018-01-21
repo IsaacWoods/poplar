bbc45eae660828719f5067a9effffaa00e1519c0
Isaac Woods <isaacwoods.home@gmail.com>
2018-01-21 20:34:14 +0000

    Separate out panic_fmt (nice place when it 'll do stack unwinding)

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -19,8 +19,8 @@
  * `rlibc` just provides intrinsics that are linked against, and so the compiler doesn't pick up
  * that it's actually used, so we suppress the warning.
  */
-#[allow(unused_extern_crates)]
-                extern crate rlibc;
+#[allow(unused_extern_crates)] extern crate rlibc;
+
                 extern crate volatile;
                 extern crate spin;
                 extern crate multiboot2;
@@ -33,24 +33,16 @@
 #[macro_use] mod x86_64;
 use x86_64 as platform;
 
+mod panic;
+pub use panic::panic_fmt;
+
 #[no_mangle]
 pub extern fn kmain(multiboot_ptr : usize)
 {
     platform::init_platform(multiboot_ptr);
-    println!("Hello, World!");
 
-    println!("ENTERING KERNEL LOOP");
+    println!("Hello, World!");
     loop { }
 }
 
-#[lang = "eh_personality"]
-extern fn eh_personality() { }
-
-#[lang = "panic_fmt"]
-#[no_mangle]
-pub extern fn panic_fmt(fmt : core::fmt::Arguments, file : &'static str, line : u32) -> !
-{
-    println!("\n\nPANIC in {} at line {}:", file, line);
-    println!("    {}", fmt);
-    loop {}
-}
+#[lang = "eh_personality"] extern fn eh_personality() { }
---
+++ kernel/src/panic.rs
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2017, Isaac Woods.
+ * See LICENCE.md
+ */
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn panic_fmt(fmt     : ::core::fmt::Arguments,
+                        file    : &'static str,
+                        line    : u32) -> !
+{
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("      {}", fmt);
+    loop {}
+}
