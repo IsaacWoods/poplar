f22db30e7461e9ab871e71008a42d070dab9e0c2
Isaac Woods <isaacwoods.home@gmail.com>
2018-03-03 13:52:12 +0000

    Mark enter_usermode naked and get interrupts in usermode working!

--- kernel/x86_64/src/user_mode.rs
+++ kernel/x86_64/src/user_mode.rs
@@ -6,24 +6,28 @@
 use memory::paging::VirtualAddress;
 use gdt::GdtSelectors;
 
+#[naked]
 pub unsafe fn enter_usermode(instruction_pointer : VirtualAddress, gdt_selectors : GdtSelectors) -> !
 {
     /*
      * TODO:
      *   * Use a real user-mode stack
-     *   * Make sure interrupts are enabled after we enter ring3
      */
     asm!("cli
           push r10      // Push selector for user data segment
           push rsp      // Push stack pointer TODO: user mode stack
-          pushfq        // Push flags (TODO: enable interrupts again (1<<9)? or just remove cli?)
-          push r11      // Push selector for user code segment
-          push r12      // Push new instruction pointer
+          push r11      // Push new RFLAGS
+          push r12      // Push selector for user code segment
+          push r13      // Push new instruction pointer
           iretq"
           :
           : "{r10}"(gdt_selectors.user_data.0),
-            "{r11}"(gdt_selectors.user_code.0),
-            "{r12}"(instruction_pointer)
+            "{r11}"(1 << 9 | 1 << 2),   // We probably shouldn't leak flags out of kernel-space, so
+                                        // we set them to the bare minimum:
+                                        //     * Bit 2 must be 1
+                                        //     * Enable interrupts by setting bit 9
+            "{r12}"(gdt_selectors.user_code.0),
+            "{r13}"(instruction_pointer)
           : // We technically don't clobber anything because this never returns
           : "intel", "volatile");
     unreachable!();
