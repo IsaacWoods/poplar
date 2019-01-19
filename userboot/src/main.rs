61938c129c3d29a813488bb279e2d68ca34f26a6
Isaac Woods <isaacwoods.home@gmail.com>
2019-01-19 00:40:06 +0000

    [userboot] Add basic userboot for testing

---
+++ userboot/src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn start() -> ! {
+    unsafe {
+        asm!("mov rax, 0xdeadbeef" :::: "intel");
+    }
+    loop {}
+}
+
+#[panic_handler]
+pub fn handle_panic(_: &PanicInfo) -> ! {
+    loop {}
+}
