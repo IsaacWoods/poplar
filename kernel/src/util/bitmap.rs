b97dc30480c27d80a620e7f44a217941670e9e5e
Isaac Woods <isaacwoods.home@gmail.com>
2019-04-04 19:38:28 +0100

    [kernel] Make the Bitmap Trait a little easier to use

--- kernel/src/util/bitmap.rs
+++ kernel/src/util/bitmap.rs
@@ -4,7 +4,7 @@
 //!
 //! A common use of the `Bitmap` trait is for memory allocators to track an area of pages, where
 //! each bit represents a page. You might, for example, want to find a series of `n` zeros (which
-//! would mark an area of `n` pages that are free to allocate) - the `alloc_n` method provides this
+//! would mark an area of `n` pages that are free to allocate) - the `alloc` method provides this
 //! functionality.
 
 use bit_field::BitField;
@@ -14,14 +14,14 @@ use num::PrimInt;
 pub trait Bitmap: Sized {
     /// Find `n` consecutive unset bits, set them and return the index of the first bit.
     /// This is useful for memory managers using `Bitmap` to track free frames / pages.
-    fn alloc_n(&mut self, n: usize) -> Option<Self>;
+    fn alloc(&mut self, n: usize) -> Option<usize>;
 }
 
 impl<T> Bitmap for T
 where
     T: PrimInt + BitField,
 {
-    fn alloc_n(&mut self, n: usize) -> Option<T> {
+    fn alloc(&mut self, n: usize) -> Option<usize> {
         let num_bits = 8 * mem::size_of::<Self>();
         let mask = Self::from((Self::one() << n) - Self::one()).unwrap();
 
@@ -33,7 +33,7 @@ where
         for i in 0..(num_bits - n) {
             if self.get_bits(i..(i + n)) & mask == Self::zero() {
                 self.set_bits(i..(i + n), mask);
-                return Some(Self::from(i).unwrap());
+                return Some(i);
             }
         }
 
@@ -42,9 +42,9 @@ where
 }
 
 #[test]
-fn test_bitmap_alloc_n() {
-    assert_eq!((0b10001: u16).alloc_n(3), Some(1));
-    assert_eq!((0b11_0000_1_000_111: u16).alloc_n(4), Some(7));
-    assert_eq!((0b1111_1111: u8).alloc_n(1), None);
-    assert_eq!((0b0110_1010: u8).alloc_n(2), None);
+fn test_bitmap_alloc() {
+    assert_eq!((0b10001: u16).alloc(3), Some(1));
+    assert_eq!((0b11_0000_1_000_111: u16).alloc(4), Some(7));
+    assert_eq!((0b1111_1111: u8).alloc(1), None);
+    assert_eq!((0b0110_1010: u8).alloc(2), None);
 }
