3aff28ebc144457541622ae60465e65854879ff0
Isaac Woods <isaacwoods.home@gmail.com>
2019-09-06 01:13:22 +0100

    Fix potential bug in InitGuard
    Not sure I 100% understand enough about atomics (and so probably shouldn't
    be writing sync primitives like this...) but I think the old version had
    a potential problem in which one CPU checks that the state is STATE_UNINIT,
    but then another CPU changes it to STATE_INITIALIZING. Both CPUs then race
    through the initialization, which obviously isn't what we want. This new
    version atomically sees if the state is STATE_UNINIT and changes it to
    STATE_INITIALIZING if it is, removing that race.

--- lib/pebble_util/src/init_guard.rs
+++ lib/pebble_util/src/init_guard.rs
@@ -33,16 +33,18 @@ impl<T> InitGuard<T> {
     /// ### Panics
     /// Panics if this `InitGuard` has already been initialized.
     pub fn initialize(&self, value: T) {
-        match self.state.load(Ordering::SeqCst) {
+        match self.state.compare_and_swap(STATE_UNINIT, STATE_INITIALIZING, Ordering::SeqCst) {
             STATE_UNINIT => {
-                self.state.store(STATE_INITIALIZING, Ordering::SeqCst);
                 unsafe {
-                    // We make sure to initialize the entire data before marking ourselves as
-                    // initialized. If we read from the data before doing this, we cause UB.
+                    /*
+                     * We make sure to initialize the entire data before marking ourselves as
+                     * initialized. If we read from the data before doing this, we cause UB.
+                     */
                     (*self.data.get()).as_mut_ptr().write(value);
                 }
                 self.state.store(STATE_INITIALIZED, Ordering::SeqCst);
             }
+
             STATE_INITIALIZING | STATE_INITIALIZED => panic!("InitGuard has already been initialized!"),
             _ => panic!("InitGuard has invalid state"),
         }
