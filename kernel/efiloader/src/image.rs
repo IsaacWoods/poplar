bb2751e7e1320f996b1a5b95f7d8eeec19720af2
Isaac Woods <isaacwoods.home@gmail.com>
2020-02-03 00:08:47 +0000

    Start work on image loading

--- kernel/efiloader/src/image.rs
+++ kernel/efiloader/src/image.rs
@@ -1,3 +1,42 @@
-pub enum ImageLoadError {}
+use uefi::{
+    prelude::*,
+    proto::media::{
+        file::{File, FileAttribute, FileInfo, FileMode, FileType},
+        fs::SimpleFileSystem,
+    },
+    table::boot::BootServices,
+    Handle,
+};
 
-pub fn load_image(path: &str) -> Result<(), ImageLoadError> {}
+#[derive(Debug)]
+pub enum ImageLoadError {
+    InvalidPath,
+    FailedToReadFile,
+}
+
+pub fn load_image(boot_services: &BootServices, volume_handle: Handle, path: &str) -> Result<(), ImageLoadError> {
+    let mut root_file_protocol = unsafe {
+        &mut *boot_services
+            .handle_protocol::<SimpleFileSystem>(volume_handle)
+            .expect_success("Failed to get volume")
+            .get()
+    }
+    .open_volume()
+    .expect_success("Failed to open volume");
+
+    let mut file = root_file_protocol.open(path, FileMode::Read, FileAttribute::READ_ONLY).unwrap_success();
+    let mut info_buffer = [0u8; 128];
+    let info = file.get_info::<FileInfo>(&mut info_buffer).unwrap_success();
+    log::info!("File info: {:?}", info.file_size());
+
+    // TODO: allocate pool of the correct size
+
+    match file.into_type().unwrap_success() {
+        FileType::Regular(regular_file) => {
+            // TODO: read data from file into pool
+        }
+        FileType::Dir(_) => return Err(ImageLoadError::InvalidPath),
+    }
+
+    Ok(())
+}
--- kernel/efiloader/src/main.rs
+++ kernel/efiloader/src/main.rs
@@ -43,12 +43,18 @@ fn efi_main(image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
     let fs_handle = find_volume(&system_table, command_line.volume_label.expect("No volume label supplied"))
         .expect("No disk with the given volume label");
 
-    if let Some(kernel_path) = command_line.kernel_path {
-        // TODO: load kernel
+    let kernel_info = if let Some(kernel_path) = command_line.kernel_path {
+        match image::load_image(system_table.boot_services(), fs_handle, kernel_path) {
+            Ok(kernel_info) => kernel_info,
+            Err(err) => {
+                error!("Failed to load kernel: {:?}", err);
+                return Status::LOAD_ERROR;
+            }
+        }
     } else {
         error!("No kernel path passed! What am I supposed to load?");
         return Status::INVALID_PARAMETER;
-    }
+    };
 
     Status::SUCCESS
 }
@@ -75,11 +81,14 @@ fn find_volume(system_table: &SystemTable<Boot>, label: &str) -> Option<Handle> 
         .locate_handle(SearchType::from_proto::<SimpleFileSystem>(), Some(handle_slice))
         .expect_success("Failed to get list of filesystems");
 
-    for handle in handle_slice {
+    // TODO: the `&mut` here is load-bearing, because we free the pool, and so need to copy the handle for if we
+    // want to return it, otherwise it disappears out from under us. This should probably be rewritten to not work
+    // like that. We could use a `Pool` type that manages the allocation and is automatically freed when dropped.
+    for &mut handle in handle_slice {
         let proto = unsafe {
             &mut *system_table
                 .boot_services()
-                .handle_protocol::<SimpleFileSystem>(*handle)
+                .handle_protocol::<SimpleFileSystem>(handle)
                 .expect_success("Failed to open SimpleFileSystem")
                 .get()
         };
@@ -89,6 +98,8 @@ fn find_volume(system_table: &SystemTable<Boot>, label: &str) -> Option<Handle> 
             .expect_success("Failed to open volume")
             .get_info::<FileSystemVolumeLabel>(&mut buffer)
             .expect_success("Failed to get volume label")
+            // TODO: maybe change uefi to take a buffer here and return a &str (allows us to remove dependency on
+            // ucs2 here for one)
             .volume_label();
 
         let mut str_buffer = [0u8; 32];
@@ -97,7 +108,7 @@ fn find_volume(system_table: &SystemTable<Boot>, label: &str) -> Option<Handle> 
 
         if volume_label_str == label {
             system_table.boot_services().free_pool(pool_addr).unwrap_success();
-            return Some(*handle);
+            return Some(handle);
         }
     }
 
@@ -107,8 +118,6 @@ fn find_volume(system_table: &SystemTable<Boot>, label: &str) -> Option<Handle> 
 
 #[panic_handler]
 fn panic_handler(info: &PanicInfo) -> ! {
-    use log::error;
-
     if let Some(location) = info.location() {
         error!("Panic in {} at ({}:{})", location.file(), location.line(), location.column());
         if let Some(message) = info.message() {
