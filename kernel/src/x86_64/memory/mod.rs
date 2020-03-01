88d74a000172f5a0ce4ca321cb90fcfa3fd0c690
Isaac Woods <isaacwoods.home@gmail.com>
2020-03-01 11:59:37 +0000

    Allocate kernel task stacks using a basic slab allocator

--- kernel/src/x86_64/memory/mod.rs
+++ kernel/src/x86_64/memory/mod.rs
@@ -4,19 +4,31 @@ mod buddy_allocator;
 pub mod userspace_map;
 
 use self::buddy_allocator::BuddyAllocator;
-use boot_info_x86_64::BootInfo;
+use boot_info_x86_64::{kernel_map, BootInfo};
 use core::ops::Range;
-use spin::{Mutex, MutexGuard};
-use x86_64::memory::{Frame, FrameAllocator};
+use pebble_util::bitmap::BitmapArray;
+use spin::Mutex;
+use x86_64::memory::{EntryFlags, Frame, FrameAllocator, Mapper, Page, VirtualAddress};
 
 const BUDDY_ALLOCATOR_MAX_ORDER: usize = 10;
 
 /// The main physical memory manager. It tracks all conventional physical memory and is used by the
 /// rest of the kernel to allocate physical memory.
-struct PhysicalMemoryManager {
+pub struct PhysicalMemoryManager {
     /// A buddy allocator used to track all conventional memory. In the future, other allocators
     /// may be used to manage a subset of memory, such as memory for DMA.
     pub buddy_allocator: BuddyAllocator,
+    pub kernel_stack_bitmap: [u8; kernel_map::MAX_TASKS / 8],
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct KernelStack {
+    pub index: usize,
+    pub top: VirtualAddress,
+    pub slot_bottom: VirtualAddress,
+    /// Depending on the initial size passed, not all of the slot may actually be mapped. This is the bottom of
+    /// the actual mapped stack.
+    pub stack_bottom: VirtualAddress,
 }
 
 pub struct LockedPhysicalMemoryManager(Mutex<PhysicalMemoryManager>);
@@ -31,7 +43,33 @@ impl LockedPhysicalMemoryManager {
             }
         }
 
-        LockedPhysicalMemoryManager(Mutex::new(PhysicalMemoryManager { buddy_allocator }))
+        LockedPhysicalMemoryManager(Mutex::new(PhysicalMemoryManager {
+            buddy_allocator,
+            kernel_stack_bitmap: [0; kernel_map::MAX_TASKS / 8],
+        }))
+    }
+
+    pub fn get_kernel_stack(&self, initial_size: usize) -> Option<KernelStack> {
+        if initial_size > kernel_map::STACK_SLOT_SIZE {
+            panic!("Tried to make kernel stack that's larger than a kernel stack slot!");
+        }
+
+        let index = self.0.lock().kernel_stack_bitmap.alloc(1)?;
+        let slot_bottom = kernel_map::KERNEL_STACKS_BASE + index * kernel_map::STACK_SLOT_SIZE;
+        let top = slot_bottom + kernel_map::STACK_SLOT_SIZE - 1;
+        let stack_bottom = top - initial_size;
+
+        for page in Page::contains(stack_bottom)..=Page::contains(top) {
+            crate::x86_64::ARCH
+                .get()
+                .kernel_page_table
+                .lock()
+                .mapper()
+                .map(page, EntryFlags::WRITABLE, self)
+                .unwrap();
+        }
+
+        Some(KernelStack { index, top, slot_bottom, stack_bottom })
     }
 }
 
