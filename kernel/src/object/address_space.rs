11af04a59ee8169ce0ecba20696a889c3473809d
Isaac Woods <isaacwoods.home@gmail.com>
2020-03-21 22:29:46 +0000

    Add methods for switching to/from and mapping stuff into an AddressSpace

--- kernel/src/object/address_space.rs
+++ kernel/src/object/address_space.rs
@@ -1,6 +1,7 @@
 use super::{alloc_kernel_object_id, memory_object::MemoryObject, KernelObject, KernelObjectId};
 use alloc::{sync::Arc, vec::Vec};
 use hal::{memory::PageTable, Hal};
+use libpebble::syscall::MemoryObjectError;
 use spin::Mutex;
 
 #[derive(PartialEq, Eq, Debug)]
@@ -36,6 +37,41 @@ where
             memory_objects: Mutex::new(vec![]),
             page_table: Mutex::new(H::PageTable::new_for_address_space(kernel_page_table, allocator)),
         }
+    }
+
+    pub fn map_memory_object(
+        &self,
+        memory_object: Arc<MemoryObject>,
+        allocator: &H::TableAllocator,
+    ) -> Result<(), MemoryObjectError> {
+        use hal::memory::PagingError;
+
+        self.page_table
+            .lock()
+            .map_area(
+                memory_object.virtual_address,
+                memory_object.physical_address,
+                memory_object.size,
+                memory_object.flags,
+                allocator,
+            )
+            .map_err(|err| match err {
+                // XXX: these are explicity enumerated to avoid a bug if variants are added to `PagingError`.
+                PagingError::AlreadyMapped => MemoryObjectError::AddressRangeNotFree,
+            })?;
+        self.memory_objects.lock().push(memory_object);
+        Ok(())
+    }
+
+    pub fn switch_to(&self) {
+        assert_eq!(*self.state.lock(), State::NotActive);
+        self.page_table.lock().switch_to();
+        *self.state.lock() = State::Active;
+    }
+
+    pub fn switch_from(&self) {
+        assert_eq!(*self.state.lock(), State::Active);
+        *self.state.lock() = State::NotActive;
     }
 }
 
