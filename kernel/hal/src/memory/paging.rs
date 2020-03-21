4b93ce67eec8a96a98cec887870ec42f796fd019
Isaac Woods <isaacwoods.home@gmail.com>
2020-03-21 20:10:51 +0000

    Start new kernel objects and implement AddressSpace again
    Start the new kernel object system for real, and start implementing the new
    AddressSpace kernel object (as this will be one of the more tricky ones)

--- kernel/hal/src/memory/paging.rs
+++ kernel/hal/src/memory/paging.rs
@@ -24,11 +24,16 @@ pub enum MapperError {
 /// A `Mapper` allows the manipulation of a set of page-tables.
 // TODO: think about how we can do versatile unmapping (maybe return a `Map` type that is returned to unmap - this
 // could store information needed to unmap an artitrarily-mapped area).
-pub trait Mapper<TableSize, TableAllocator>
+pub trait Mapper<TableSize, TableAllocator>: Sized
 where
     TableSize: FrameSize,
     TableAllocator: FrameAllocator<TableSize>,
 {
+    /// Construct a new set of page tables that are suitable for an `AddressSpace` kernel object - one that can
+    /// hold userspace tasks. This generally needs the kernel mapped into it somehow, so we pass in the kernel's
+    /// set of page tables.
+    fn new_for_address_space(kernel_page_table: &Self, allocator: &TableAllocator) -> Self;
+
     /// Get the physical address that a given virtual address is mapped to, if it's mapped. Returns `None` if the
     /// address is not mapped into physical memory.
     fn translate(&self, address: VirtualAddress) -> Option<PhysicalAddress>;
---
+++ kernel/src/object/address_space.rs
@@ -0,0 +1,49 @@
+use super::{alloc_kernel_object_id, memory_object::MemoryObject, KernelObject, KernelObjectId};
+use alloc::{sync::Arc, vec::Vec};
+use hal::{memory::Mapper, Hal};
+use spin::Mutex;
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum State {
+    NotActive,
+    Active,
+}
+
+pub struct AddressSpace<H>
+where
+    H: Hal,
+{
+    pub id: KernelObjectId,
+    pub owner: KernelObjectId,
+    pub state: Mutex<State>,
+    pub memory_objects: Mutex<Vec<Arc<MemoryObject>>>,
+    page_table: Mutex<H::PageTable>,
+}
+
+impl<H> AddressSpace<H>
+where
+    H: Hal,
+{
+    pub fn new(
+        owner: KernelObjectId,
+        kernel_page_table: &H::PageTable,
+        allocator: &H::TableAllocator,
+    ) -> AddressSpace<H> {
+        AddressSpace {
+            id: alloc_kernel_object_id(),
+            owner,
+            state: Mutex::new(State::NotActive),
+            memory_objects: Mutex::new(vec![]),
+            page_table: Mutex::new(H::PageTable::new_for_address_space(kernel_page_table, allocator)),
+        }
+    }
+}
+
+impl<H> KernelObject for AddressSpace<H>
+where
+    H: Hal,
+{
+    fn id(&self) -> KernelObjectId {
+        self.id
+    }
+}
