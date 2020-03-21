687550ecaa0c810aeac7d70f7109d81f455d4d1b
Isaac Woods <isaacwoods.home@gmail.com>
2020-03-21 22:54:01 +0000

    Add common kernel entry point
    The entry point to the kernel can now be in common code, as opposed to a
    arch-specific entry point that hands control over at some point! All the
    init code can now move into the HAL layer.

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -19,68 +19,89 @@
 #[macro_use]
 extern crate alloc;
 
-/*
- * This selects the correct module to include depending on the architecture we're compiling the
- * kernel for. These architecture modules contain the kernel entry point and any
- * platform-specific code.
- */
-cfg_if! {
-    if #[cfg(feature = "arch_x86_64")] {
-        mod x86_64;
-        use crate::x86_64 as arch_impl;
-        pub use crate::x86_64::kmain;
-    } else {
-        compile_error!("Tried to build kernel without specifying an architecture!");
-    }
-}
-
 mod heap_allocator;
 mod mailbox;
 mod object;
-mod per_cpu;
-mod scheduler;
+// mod per_cpu;
+// mod scheduler;
 mod syscall;
 
-use crate::{heap_allocator::LockedHoleAllocator, object::map::ObjectMap};
+use crate::heap_allocator::LockedHoleAllocator;
 use cfg_if::cfg_if;
 use core::panic::PanicInfo;
+use hal::boot_info::BootInfo;
 use libpebble::{syscall::system_object::FramebufferSystemObjectInfo, KernelObjectId};
-use log::error;
-use pebble_util::InitGuard;
-use spin::{Mutex, RwLock};
+use log::{error, info};
+
+cfg_if! {
+    if #[cfg(feature = "arch_x86_64")] {
+        type HalImpl = hal_x86_64::HalImpl;
+
+        // TODO: method for constructing HAL impl
+    } else {
+        compile_error!("No architecture supplied, or target arch does not have a HAL implementation configured!");
+    }
+}
 
 #[cfg(not(test))]
 #[global_allocator]
 pub static ALLOCATOR: LockedHoleAllocator = LockedHoleAllocator::new_uninitialized();
 
-/// We need to make various bits of data accessible on a system-wide level (all the CPUs access the
-/// same data), including from system call and interrupt handlers. I haven't discovered a
-/// particularly elegant way of doing that in Rust yet, but this isn't totally awful.
-///
-/// This can be accessed from anywhere in the kernel, and from any CPU, and so access to each member
-/// must be controlled by a type such as `Mutex` or `RwLock`. This has lower lock contention than
-/// locking the entire structure.
-pub static COMMON: InitGuard<Common> = InitGuard::uninit();
+#[no_mangle]
+pub extern "C" fn kmain(boot_info: &BootInfo) -> ! {
+    // TODO: common logger
+    /*
+     * Initialise the logger.
+     */
+    // log::set_logger(&KernelLogger).unwrap();
+    log::set_max_level(log::LevelFilter::Trace);
+    info!("The Pebble kernel is running");
 
-/// This is a collection of stuff we need to access from around the kernel, shared between all
-/// CPUs. This has the potential to end up as a bit of a "God struct", so we need to be careful.
-pub struct Common {
-    pub object_map: RwLock<ObjectMap<arch_impl::Arch>>,
-
-    /// If the bootloader switched to a graphics mode that enables the use of a linear framebuffer,
-    /// this kernel object will be a MemoryObject that maps the backing memory into a userspace
-    /// driver. This is provided to userspace through the `request_system_object` system call.
-    pub backup_framebuffer: Mutex<Option<(KernelObjectId, FramebufferSystemObjectInfo)>>,
-}
-
-impl Common {
-    pub fn new() -> Common {
-        Common {
-            object_map: RwLock::new(ObjectMap::new(crate::object::map::INITIAL_OBJECT_CAPACITY)),
-            backup_framebuffer: Mutex::new(None),
-        }
+    if boot_info.magic != hal::boot_info::BOOT_INFO_MAGIC {
+        panic!("Boot info magic is not correct!");
     }
+
+    /*
+     * Initialise the heap allocator. After this, the kernel is free to use collections etc. that
+     * can allocate on the heap through the global allocator.
+     */
+    unsafe {
+        #[cfg(not(test))]
+        ALLOCATOR.lock().init(boot_info.heap_address, boot_info.heap_size);
+    }
+
+    // TODO: initialise the HAL and start doing stuff
+    loop {}
 }
+
+///// We need to make various bits of data accessible on a system-wide level (all the CPUs access the
+///// same data), including from system call and interrupt handlers. I haven't discovered a
+///// particularly elegant way of doing that in Rust yet, but this isn't totally awful.
+/////
+///// This can be accessed from anywhere in the kernel, and from any CPU, and so access to each member
+///// must be controlled by a type such as `Mutex` or `RwLock`. This has lower lock contention than
+///// locking the entire structure.
+//pub static COMMON: InitGuard<Common> = InitGuard::uninit();
+
+// /// This is a collection of stuff we need to access from around the kernel, shared between all
+// /// CPUs. This has the potential to end up as a bit of a "God struct", so we need to be careful.
+// pub struct Common {
+//     pub object_map: RwLock<ObjectMap<arch_impl::Arch>>,
+
+//     /// If the bootloader switched to a graphics mode that enables the use of a linear framebuffer,
+//     /// this kernel object will be a MemoryObject that maps the backing memory into a userspace
+//     /// driver. This is provided to userspace through the `request_system_object` system call.
+//     pub backup_framebuffer: Mutex<Option<(KernelObjectId, FramebufferSystemObjectInfo)>>,
+// }
+
+// impl Common {
+//     pub fn new() -> Common {
+//         Common {
+//             object_map: RwLock::new(ObjectMap::new(crate::object::map::INITIAL_OBJECT_CAPACITY)),
+//             backup_framebuffer: Mutex::new(None),
+//         }
+//     }
+// }
 
 #[cfg(not(test))]
 #[panic_handler]
