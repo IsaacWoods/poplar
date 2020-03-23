7f1d3c47c76c175edf36adc1d35da7b4d35939c7
Isaac Woods <isaacwoods.home@gmail.com>
2020-03-23 19:50:13 +0000

    Initialize HAL in common and init kernel logger

--- kernel/hal/src/lib.rs
+++ kernel/hal/src/lib.rs
@@ -4,12 +4,16 @@
 pub mod boot_info;
 pub mod memory;
 
+use boot_info::BootInfo;
 use memory::{FrameAllocator, FrameSize, PageTable};
 
-pub trait Hal {
+pub trait Hal: Sized {
     type PageTableSize: FrameSize;
     type TableAllocator: FrameAllocator<Self::PageTableSize>;
     type PageTable: PageTable<Self::PageTableSize, Self::TableAllocator>;
+
+    fn init_logger();
+    fn new(boot_info: &BootInfo) -> Self;
 
     unsafe fn disable_interrupts();
     unsafe fn enable_interrupts();
