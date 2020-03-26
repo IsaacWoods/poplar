13bed93ebf62eafa25ca2224f2c785054ae434c9
Isaac Woods <isaacwoods.home@gmail.com>
2020-03-26 20:49:22 +0000

    Quick'n'dirty user stack allocation
    Depending on what the actual address spaces end up looking like on each
    architecture, we might need to get the available ranges from the HAL or
    something. Maybe we should make it more dynamic too?

--- kernel/src/object/address_space.rs
+++ kernel/src/object/address_space.rs
@@ -1,8 +1,18 @@
-use super::{alloc_kernel_object_id, memory_object::MemoryObject, KernelObject, KernelObjectId};
+use super::{alloc_kernel_object_id, memory_object::MemoryObject, task::TaskStack, KernelObject, KernelObjectId};
+use crate::slab_allocator::SlabAllocator;
 use alloc::{sync::Arc, vec::Vec};
-use hal::{memory::PageTable, Hal};
+use hal::{
+    memory::{PageTable, VirtualAddress, MEBIBYTES_TO_BYTES},
+    Hal,
+};
 use libpebble::syscall::MemoryObjectError;
 use spin::Mutex;
+
+// TODO: we need some way of getting this from the platform I guess?
+// TODO: we've basically made these up
+const USER_STACK_BOTTOM: VirtualAddress = VirtualAddress::new(0x00000002_00000000);
+const USER_STACK_TOP: VirtualAddress = VirtualAddress::new(0x00000003_ffffffff);
+const USER_STACK_SLOT_SIZE: usize = 2 * MEBIBYTES_TO_BYTES;
 
 #[derive(PartialEq, Eq, Debug)]
 pub enum State {
@@ -19,6 +29,7 @@ where
     pub state: Mutex<State>,
     pub memory_objects: Mutex<Vec<Arc<MemoryObject>>>,
     page_table: Mutex<H::PageTable>,
+    user_stack_allocator: Mutex<SlabAllocator>,
 }
 
 impl<H> AddressSpace<H>
@@ -36,6 +47,11 @@ where
             state: Mutex::new(State::NotActive),
             memory_objects: Mutex::new(vec![]),
             page_table: Mutex::new(H::PageTable::new_for_address_space(kernel_page_table, allocator)),
+            user_stack_allocator: Mutex::new(SlabAllocator::new(
+                USER_STACK_BOTTOM,
+                USER_STACK_TOP,
+                USER_STACK_SLOT_SIZE,
+            )),
         }
     }
 
@@ -61,6 +77,32 @@ where
             })?;
         self.memory_objects.lock().push(memory_object);
         Ok(())
+    }
+
+    /// Try to allocate a slot for a user stack, and map `initial_size` bytes of it. Returns `None` if no more user
+    /// stacks can be allocated in this address space.
+    // TODO: feels gross allocating out of the TableAllocator - maybe rename it in the HAL?
+    pub fn alloc_user_stack(&self, initial_size: usize, allocator: &H::TableAllocator) -> Option<TaskStack> {
+        use hal::memory::{Flags, FrameAllocator, FrameSize, PageTable};
+
+        let slot_bottom = self.user_stack_allocator.lock().alloc()?;
+        let top = slot_bottom + USER_STACK_SLOT_SIZE - 1;
+        let stack_bottom = top - initial_size + 1;
+
+        // TODO: nasty
+        let physical_start = allocator.allocate_n(H::PageTableSize::frames_needed(initial_size)).start.start;
+        self.page_table
+            .lock()
+            .map_area(
+                stack_bottom,
+                physical_start,
+                initial_size,
+                Flags { writable: true, user_accessible: true, ..Default::default() },
+                allocator,
+            )
+            .unwrap();
+
+        Some(TaskStack { top, slot_bottom, stack_bottom })
     }
 
     pub fn switch_to(&self) {
