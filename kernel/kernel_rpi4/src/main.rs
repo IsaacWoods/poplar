e0a9a24a62908ed120bef03dcd29f1109fb4fe73
Isaac Woods <isaacwoods.home@gmail.com>
2020-05-07 17:42:42 +0100

    kernel_rpi4: start boilerplate for Raspberry Pi 4 kernel

---
+++ kernel/kernel_rpi4/src/main.rs
@@ -0,0 +1,38 @@
+#![no_std]
+#![no_main]
+#![feature(llvm_asm, global_asm)]
+
+global_asm!(include_str!("start.s"));
+
+use core::panic::PanicInfo;
+
+extern "C" {
+    static __bss_start: usize;
+    static __bss_end: usize;
+}
+
+#[no_mangle]
+pub fn kmain() -> ! {
+    // Zero BSS
+    // TODO: do this in assembly before we reach Rust
+    let mut ptr = unsafe { &__bss_start as *const _ as *mut usize };
+    let end = unsafe { &__bss_end as *const _ as *mut usize };
+
+    while ptr < end {
+        unsafe {
+            core::ptr::write_volatile(ptr, 0x0);
+            ptr = ptr.offset(1);
+        }
+    }
+
+    panic!("Did stuff!");
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        unsafe {
+            llvm_asm!("wfe");
+        }
+    }
+}
