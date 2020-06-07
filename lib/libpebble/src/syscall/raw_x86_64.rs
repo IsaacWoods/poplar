758a62e65201d57a7e8d20d2e4ddeeff73249ec0
Isaac Woods <isaacwoods.home@gmail.com>
2020-06-07 21:06:29 +0100

    libpebble: syscall writes into rcx and r11 so we should clobber them

--- lib/libpebble/src/syscall/raw_x86_64.rs
+++ lib/libpebble/src/syscall/raw_x86_64.rs
@@ -4,6 +4,8 @@ pub unsafe fn syscall0(number: usize) -> usize {
     asm!("syscall",
         out("rax") result,
         in("rdi") number,
+        out("rcx") _,
+        out("r11") _,
     );
     result
 }
@@ -15,6 +17,8 @@ pub unsafe fn syscall1(number: usize, a: usize) -> usize {
         out("rax") result,
         in("rdi") number,
         in("rsi") a,
+        out("rcx") _,
+        out("r11") _,
     );
     result
 }
@@ -27,6 +31,8 @@ pub unsafe fn syscall2(number: usize, a: usize, b: usize) -> usize {
         in("rdi") number,
         in("rsi") a,
         in("rdx") b,
+        out("rcx") _,
+        out("r11") _,
     );
     result
 }
@@ -40,6 +46,8 @@ pub unsafe fn syscall3(number: usize, a: usize, b: usize, c: usize) -> usize {
         in("rsi") a,
         in("rdx") b,
         in("r10") c,
+        out("rcx") _,
+        out("r11") _,
     );
     result
 }
@@ -54,6 +62,8 @@ pub unsafe fn syscall4(number: usize, a: usize, b: usize, c: usize, d: usize) ->
         in("rdx") b,
         in("r10") c,
         in("r8") d,
+        out("rcx") _,
+        out("r11") _,
     );
     result
 }
@@ -69,6 +79,8 @@ pub unsafe fn syscall5(number: usize, a: usize, b: usize, c: usize, d: usize, e:
         in("r10") c,
         in("r8") d,
         in("r9") e,
+        out("rcx") _,
+        out("r11") _,
     );
     result
 }
