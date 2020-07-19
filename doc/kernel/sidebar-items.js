9a1402770a147bb31acd6a735f1adab9dd60d106
IsaacWoods <isaacwoods0@protonmail.com>
2020-07-19 12:39:07 +0000

    Deploying to gh-pages from  @ 4dfeded96d3f551a01547f08d06bc26a8414fc2d 🚀

--- doc/kernel/sidebar-items.js
+++ doc/kernel/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["create_framebuffer",""],["load_task",""]],"mod":[["heap_allocator",""],["memory",""],["object",""],["per_cpu",""],["scheduler",""],["slab_allocator",""],["syscall",""]],"static":[["ALLOCATOR",""],["FRAMEBUFFER",""],["PHYSICAL_MEMORY_MANAGER",""]],"trait":[["Platform",""]]});
+initSidebarItems({"fn":[["create_framebuffer",""],["load_task",""]],"mod":[["heap_allocator",""],["memory",""],["object",""],["per_cpu",""],["scheduler",""],["syscall",""]],"static":[["ALLOCATOR",""],["FRAMEBUFFER",""],["PHYSICAL_MEMORY_MANAGER",""]],"trait":[["Platform",""]]});
