a13ef11d20986d8e6fcd3d0d661c4ade70b27091
Isaac Woods <isaacwoods.home@gmail.com>
2020-08-13 15:08:22 +0100

    gfxconsole: abstract out framebuffer drawing code into crate
    Turns out we want to render to a framebuffer from multiple places, so it's
    probably worth just creating a crate with all the nice stuff in.

--- drivers/simple_fb/src/main.rs
+++ drivers/simple_fb/src/main.rs
@@ -5,9 +5,8 @@
 extern crate alloc;
 extern crate rlibc;
 
-use bit_field::BitField;
 use core::{mem::MaybeUninit, panic::PanicInfo};
-use font8x8::UnicodeFonts;
+use gfxconsole::{Bgr32, Format, Framebuffer, Pixel};
 use libpebble::{
     caps::{CapabilitiesRepr, CAP_EARLY_LOGGING, CAP_GET_FRAMEBUFFER, CAP_PADDING},
     early_logger::EarlyLogger,
@@ -19,74 +18,28 @@ use log::info;
 #[global_allocator]
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
-pub struct Framebuffer {
-    pointer: *mut u32,
-    width: usize,
-    height: usize,
-    stride: usize,
-}
+fn make_framebuffer() -> Framebuffer<Bgr32> {
+    let (framebuffer_handle, framebuffer_info) = {
+        let mut framebuffer_info: MaybeUninit<FramebufferInfo> = MaybeUninit::uninit();
 
-impl Framebuffer {
-    pub fn new() -> Framebuffer {
-        let (framebuffer_handle, framebuffer_info) = {
-            let mut framebuffer_info: MaybeUninit<FramebufferInfo> = MaybeUninit::uninit();
+        let framebuffer_handle =
+            syscall::get_framebuffer(framebuffer_info.as_mut_ptr()).expect("Failed to get handle to framebuffer!");
 
-            let framebuffer_handle = syscall::get_framebuffer(framebuffer_info.as_mut_ptr())
-                .expect("Failed to get handle to framebuffer!");
+        (framebuffer_handle, unsafe { framebuffer_info.assume_init() })
+    };
 
-            (framebuffer_handle, unsafe { framebuffer_info.assume_init() })
-        };
+    let mut framebuffer_address: MaybeUninit<usize> = MaybeUninit::uninit();
+    syscall::map_memory_object(framebuffer_handle, libpebble::ZERO_HANDLE, framebuffer_address.as_mut_ptr())
+        .unwrap();
+    let framebuffer_address = unsafe { framebuffer_address.assume_init() };
 
-        let mut framebuffer_address: MaybeUninit<usize> = MaybeUninit::uninit();
-        syscall::map_memory_object(framebuffer_handle, libpebble::ZERO_HANDLE, framebuffer_address.as_mut_ptr())
-            .unwrap();
-        let framebuffer_address = unsafe { framebuffer_address.assume_init() };
+    assert_eq!(framebuffer_info.pixel_format, PixelFormat::BGR32);
 
-        assert_eq!(framebuffer_info.pixel_format, PixelFormat::BGR32);
-
-        Framebuffer {
-            pointer: framebuffer_address as *mut u32,
-            width: framebuffer_info.width as usize,
-            height: framebuffer_info.height as usize,
-            stride: framebuffer_info.stride as usize,
-        }
-    }
-
-    pub fn draw_rect(&self, start_x: usize, start_y: usize, width: usize, height: usize, color: u32) {
-        assert!((start_x + width) <= self.width);
-        assert!((start_y + height) <= self.height);
-
-        for y in start_y..(start_y + height) {
-            for x in start_x..(start_x + width) {
-                unsafe {
-                    *(self.pointer.offset((y * self.stride + x) as isize)) = color;
-                }
-            }
-        }
-    }
-
-    pub fn clear(&self, color: u32) {
-        self.draw_rect(0, 0, self.width, self.height, color);
-    }
-
-    pub fn draw_glyph(&self, key: char, x: usize, y: usize, color: u32) {
-        for (line, line_data) in font8x8::BASIC_FONTS.get(key).unwrap().iter().enumerate() {
-            // TODO: this is amazingly inefficient. We could replace with a lookup table and multiply by the color
-            // if this is too slow.
-            for bit in 0..8 {
-                if line_data.get_bit(bit) {
-                    unsafe {
-                        *(self.pointer.offset(((y + line) * self.stride + (x + bit)) as isize)) = color;
-                    }
-                }
-            }
-        }
-    }
-
-    pub fn draw_string(&self, string: &str, start_x: usize, start_y: usize, color: u32) {
-        for (index, c) in string.chars().enumerate() {
-            self.draw_glyph(c, start_x + (index * 8), start_y, color);
-        }
+    Framebuffer {
+        ptr: framebuffer_address as *mut Pixel<Bgr32>,
+        width: framebuffer_info.width as usize,
+        height: framebuffer_info.height as usize,
+        stride: framebuffer_info.stride as usize,
     }
 }
 
@@ -106,14 +59,14 @@ pub extern "C" fn start() -> ! {
     log::set_max_level(log::LevelFilter::Trace);
     info!("Simple framebuffer driver is running!");
 
-    let framebuffer = Framebuffer::new();
-    framebuffer.clear(0xffaaaaaa);
-    framebuffer.draw_rect(100, 100, 300, 450, 0xffcc0000);
+    let framebuffer = make_framebuffer();
+    framebuffer.clear(Bgr32::pixel(0xffaaaaaa));
+    framebuffer.draw_rect(100, 100, 300, 450, Bgr32::pixel(0xffcc0000));
     framebuffer.draw_string(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         200,
         400,
-        0xff000000,
+        Bgr32::pixel(0xff000000),
     );
 
     loop {
--- kernel/kernel_x86_64/src/main.rs
+++ kernel/kernel_x86_64/src/main.rs
@@ -78,6 +78,19 @@ pub extern "C" fn kentry(boot_info: &BootInfo) -> ! {
         panic!("Boot info magic is not correct!");
     }
 
+    use gfxconsole::{Format, Framebuffer, Pixel, Rgb32};
+    let framebuffer = Framebuffer {
+        ptr: hal_x86_64::kernel_map::physical_to_virtual(
+            boot_info.video_mode.as_ref().unwrap().framebuffer_address,
+        )
+        .mut_ptr() as *mut Pixel<Rgb32>,
+        width: boot_info.video_mode.as_ref().unwrap().width,
+        height: boot_info.video_mode.as_ref().unwrap().height,
+        stride: boot_info.video_mode.as_ref().unwrap().stride,
+    };
+    framebuffer.clear(Rgb32::pixel(0xffff00ff));
+    framebuffer.draw_string("Hello from the kernel", 100, 100, Rgb32::pixel(0xff0000ff));
+
     /*
      * Initialise the heap allocator. After this, the kernel is free to use collections etc. that
      * can allocate on the heap through the global allocator.
