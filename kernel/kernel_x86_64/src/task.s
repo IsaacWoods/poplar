55c155baebc3db5fdf6a9a9a975dd2b0b7eaa4ea
Isaac Woods <isaacwoods.home@gmail.com>
2020-08-16 18:43:51 +0100

    kernel_x86_64: disable interrupts when entering a task
    We do a bit of messing about with stacks and whatnot in the task entry
    trampoline, so it's safer to have interrupts disabled. They are re-enabled
    by `sysretq` when it loads a new set of flags.
    
    Also reformats some comments in the same file.

--- kernel/kernel_x86_64/src/task.s
+++ kernel/kernel_x86_64/src/task.s
@@ -16,6 +16,9 @@
  */
 .global task_entry_trampoline
 task_entry_trampoline:
+    // Disable interrupts while we're messing around with stacks. Re-enabled on `sysretq`.
+    cli
+
     mov gs:0x8, rsp     // Save the task's kernel stack
     mov rsp, gs:0x10
 
@@ -39,8 +42,7 @@ task_entry_trampoline:
 // fn do_drop_into_usermode() -> !
 .global do_drop_to_usermode
 do_drop_to_usermode:
-    // Disable interrupts so we aren't interrupted in a weird state. They are reenabled when the new flags are
-    // loaded on `sysret`
+    // Disable interrupts while we're messing around with stacks. Re-enabled on `sysretq`.
     cli
 
     // Switch to the task's kernel stack
@@ -58,9 +60,11 @@ do_drop_to_usermode:
     // Switch to the task's user stack
     mov rsp, gs:0x10
 
-    // Zero all registers that weren't zerod as part of the context load, except rcx and r11, as they're needed by
-    // `sysret`. We also zero `r14` and `r15`, which would normally be loaded from the saved context but weren't
-    // because we use them to populate `r11` and `rcx` instead.
+    /*
+     * Zero all registers that weren't zerod as part of the context load, except rcx and r11, as they're needed by
+     * `sysret`. We also zero `r14` and `r15`, which would normally be loaded from the saved context but weren't
+     * because we use them to populate `r11` and `rcx` instead.
+     */
     xor rax, rax
     xor rdx, rdx
     xor rsi, rsi
