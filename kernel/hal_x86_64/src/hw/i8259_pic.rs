2c73fdcd3e176e337726a8e7283d8fd752e5343b
Isaac Woods <isaacwoods.home@gmail.com>
2020-09-07 15:24:22 +0100

    hal_x86_64: rename 'master' and 'slave' PICs
    I didn't realise we still called the i8259s by these names, so replace
    with 'primary' and 'secondary' instead. This is a weighup, because the
    official manuals and many other sources still refer to them by 'master' and
    'slave'. 'Primary' and 'secondary' seems more appropriate anyways, since
    the 'primary' doesn't really have control over the 'secondary', one is
    simply chained into the other.

--- kernel/hal_x86_64/src/hw/i8259_pic.rs
+++ kernel/hal_x86_64/src/hw/i8259_pic.rs
@@ -1,60 +1,59 @@
 use super::port::Port;
 
 pub struct Pic {
-    master_command: Port<u8>,
-    master_data: Port<u8>,
-    slave_command: Port<u8>,
-    slave_data: Port<u8>,
+    primary_command: Port<u8>,
+    primary_data: Port<u8>,
+    secondary_command: Port<u8>,
+    secondary_data: Port<u8>,
 }
 
 impl Pic {
     pub const unsafe fn new() -> Pic {
         Pic {
-            master_command: Port::new(0x20),
-            master_data: Port::new(0x21),
-            slave_command: Port::new(0xa0),
-            slave_data: Port::new(0xa1),
+            primary_command: Port::new(0x20),
+            primary_data: Port::new(0x21),
+            secondary_command: Port::new(0xa0),
+            secondary_data: Port::new(0xa1),
         }
     }
 
-    /// Remap and disable the PIC. It is necessary to remap the PIC even if we don't want to use it
-    /// because otherwise spurious interrupts can cause exceptions.
-    pub fn remap_and_disable(&mut self, master_vector_offset: u8, slave_vector_offset: u8) {
+    /// Remap and disable the PIC. It is necessary to remap the PIC even if we don't want to use it because
+    /// otherwise spurious interrupts can cause exceptions.
+    pub fn remap_and_disable(&mut self, primary_vector_offset: u8, secondary_vector_offset: u8) {
         unsafe {
             /*
-             * 0x80 is a port used by POST. It shouldn't do anything, but it'll take long enough
-             * to execute writes to it that we should block for long enough for the
-             * PICs to actually do what we ask them to.
+             * 0x80 is a port used by POST. It shouldn't do anything, but it'll take long enough to execute writes
+             * to it that we should block for long enough for the PICs to actually do what we ask them to.
              */
             let mut wait_port: Port<u8> = Port::new(0x80);
             let mut wait = || wait_port.write(0);
 
             // Tell the PICs to start their initialization sequences in cascade mode
-            self.master_command.write(0x11);
-            self.slave_command.write(0x11);
+            self.primary_command.write(0x11);
+            self.secondary_command.write(0x11);
             wait();
 
             // Tell the PICs their new interrupt vectors
-            self.master_data.write(master_vector_offset);
-            self.slave_data.write(slave_vector_offset);
+            self.primary_data.write(primary_vector_offset);
+            self.secondary_data.write(secondary_vector_offset);
             wait();
 
-            // Tell the master PIC that the slave is at IRQ2
-            self.master_data.write(0b100);
+            // Tell the primary PIC that the secondary is at IRQ2
+            self.primary_data.write(0b100);
             wait();
 
-            // Tell the slave PIC its cascade identity
-            self.slave_data.write(0b10);
+            // Tell the secondary PIC its cascade identity
+            self.secondary_data.write(0b10);
             wait();
 
             // Tell the PICs to go into 8086/88 MCS-80/85 mode
-            self.master_data.write(0x1);
-            self.slave_data.write(0x1);
+            self.primary_data.write(0x1);
+            self.secondary_data.write(0x1);
             wait();
 
             // Mask both PICs
-            self.master_data.write(0xff);
-            self.slave_data.write(0xff);
+            self.primary_data.write(0xff);
+            self.secondary_data.write(0xff);
         }
     }
 }
