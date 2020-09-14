811179c171afedc182c8d7f9b6e047c8375a88cf
Isaac Woods <isaacwoods.home@gmail.com>
2020-09-14 17:37:59 +0100

    ptah: set out start of basic integration testing

---
+++ lib/ptah/tests/wellformed.rs
@@ -0,0 +1,173 @@
+#![feature(type_ascription)]
+
+use ptah::CursorWriter;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::fmt::Debug;
+
+const BUFFER_SIZE: usize = 128;
+
+fn test_value<T>(value: T)
+where
+    T: Serialize + DeserializeOwned + PartialEq + Debug,
+{
+    /*
+     * The stdout output will only actually be printed if a test fails, so we print some stuff that might be useful
+     * in debugging a failing case.
+     */
+    let mut buffer = [0u8; BUFFER_SIZE];
+    match ptah::to_wire(&value, CursorWriter::new(&mut buffer)) {
+        Ok(()) => (),
+        Err(err) => panic!("Failed to serialize value: {:?} (err = {:?})", value, err),
+    }
+    println!("Encoded: {:x?}", buffer);
+    let size = match ptah::serialized_size(&value) {
+        Ok(size) => size,
+        Err(err) => panic!("Failed to calculate serialized size of value: {:?} (err = {:?})", value, err),
+    };
+    println!("Calculated size of value {:?}: {}", value, size);
+    let decoded = match ptah::from_wire(&buffer[0..size]) {
+        Ok(value) => value,
+        Err(err) => panic!("Failed to deserialize value: {:?} (err = {:?})", value, err),
+    };
+    assert_eq!(value, decoded);
+}
+
+#[test]
+fn numbers() {
+    test_value(91u8);
+    test_value(412u16);
+    test_value(912u32);
+    test_value(0xf4f4_8273_e5a3_2f54u64);
+    test_value(0xf4f4_8273_e5a3_2f54usize);
+
+    test_value(-5i8);
+    test_value(345i16);
+    test_value(-23556i32);
+    test_value(-67i64);
+    test_value(-4562isize);
+
+    test_value(34.6f32);
+    test_value(-192.3245f64);
+}
+
+#[test]
+fn strings() {
+    test_value("".to_string());
+    test_value("Hello, World!".to_string());
+}
+
+#[test]
+fn test_simple_structs() {
+    #[derive(Clone, Copy, PartialEq, Debug, Serialize, Deserialize)]
+    struct Foo {
+        a: u8,
+        b: usize,
+        c: f64,
+    }
+
+    test_value(Foo { a: 0, b: 43, c: 28.99 });
+
+    #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+    struct Bar {
+        thing_on_heap: String,
+        other_heap_thing: Vec<u16>,
+        just_a_number: usize,
+    }
+
+    test_value(Bar {
+        thing_on_heap: "Serde is pretty cool".to_string(),
+        other_heap_thing: vec![9, 14, 66, 34, 0],
+        just_a_number: 11,
+    });
+}
+
+#[test]
+fn test_nested_structs() {
+    #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+    struct Egg {
+        foo: String,
+        bar: usize,
+    }
+
+    #[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+    struct Nest {
+        egg: Egg,
+        a: f64,
+        other_egg: Egg,
+        yet_another_egg: Egg,
+    }
+
+    test_value(Nest {
+        egg: Egg { foo: "Egg One".to_string(), bar: 43 },
+        a: 3.14159265,
+        other_egg: Egg { foo: "Egg B".to_string(), bar: 963 },
+        yet_another_egg: Egg { foo: "Tertiary Egg".to_string(), bar: 7 },
+    });
+}
+
+#[test]
+fn test_newtype_struct() {
+    #[derive(Clone, Copy, PartialEq, Eq, Debug, Serialize, Deserialize)]
+    #[repr(transparent)]
+    struct Foo(u16);
+
+    test_value(Foo(8));
+}
+
+#[test]
+fn test_tuple_struct() {
+    #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+    struct Foo(u8, String, ());
+
+    test_value(Foo(9, "Foo".to_string(), ()));
+}
+
+#[test]
+fn test_unit() {
+    test_value(());
+}
+
+#[test]
+fn test_tuples() {
+    test_value((11,));
+    test_value((0.0f32, 73, "Foo".to_string(), -6));
+}
+
+#[test]
+fn test_options() {
+    test_value(None: Option<usize>);
+    test_value(Some(6));
+    test_value(Some("Hello, World!".to_string()));
+
+    #[derive(Clone, Copy, PartialEq, Eq, Debug, Serialize, Deserialize)]
+    struct Foo {
+        a: usize,
+        b: u8,
+    }
+
+    test_value(Some(Foo { a: 0, b: 43 }));
+}
+
+#[test]
+fn test_enums() {
+    #[derive(Clone, Copy, PartialEq, Eq, Debug, Serialize, Deserialize)]
+    enum Foo {
+        A,
+        B,
+        C,
+        D,
+    }
+    test_value(Foo::A);
+    test_value(Foo::B);
+    test_value(Foo::C);
+    test_value(Foo::D);
+
+    #[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+    enum Bar {
+        None,
+        Some(Vec<usize>),
+    }
+
+    test_value(Bar::None);
+    test_value(Bar::Some(vec![654, 9]));
+}
