0f315f189fd741647274b1bf5781cc06178a3d24
Isaac Woods <isaacwoods.home@gmail.com>
2020-09-15 16:26:24 +0100

    drivers: actually use ptah from userspace!!
    This is pretty exciting - all of basic pieces are coming together here. One
    usermode task is subscribing to another, which provides the `echo` service,
    and sending it messages serialized using our custom data format, Ptah, which
    are then correctly echoed back and deserialized!

--- drivers/simple_fb/src/main.rs
+++ drivers/simple_fb/src/main.rs
@@ -5,6 +5,10 @@
 extern crate alloc;
 extern crate rlibc;
 
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
 use core::{mem::MaybeUninit, panic::PanicInfo};
 use gfxconsole::{Bgr32, Format, Framebuffer, Pixel};
 use libpebble::{
@@ -14,9 +18,78 @@ use libpebble::{
 };
 use linked_list_allocator::LockedHeap;
 use log::info;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TestMessage {
+    id: usize,
+    message: String,
+}
 
 #[global_allocator]
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+#[no_mangle]
+pub extern "C" fn start() -> ! {
+    syscall::early_log("Hello from FB").unwrap();
+    // Initialise the heap
+    const HEAP_START: usize = 0x600000000;
+    const HEAP_SIZE: usize = 0x4000;
+    let heap_memory_object = syscall::create_memory_object(HEAP_START, HEAP_SIZE, true, false).unwrap();
+    syscall::map_memory_object(heap_memory_object, libpebble::ZERO_HANDLE, 0x0 as *mut usize).unwrap();
+    unsafe {
+        ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE);
+    }
+
+    log::set_logger(&EarlyLogger).unwrap();
+    log::set_max_level(log::LevelFilter::Trace);
+    info!("Simple framebuffer driver is running!");
+
+    /*
+     * Test out the service stuff. We want the echo service.
+     */
+    let echo_channel = syscall::subscribe_to_service("echo.echo").expect("Failed to subscribe to echo service :(");
+    let message = {
+        let mut message_bytes = Vec::new();
+        ptah::to_wire(&TestMessage { id: 43, message: "Hello, World!".to_string() }, &mut message_bytes).unwrap();
+        message_bytes
+    };
+    syscall::send_message(echo_channel, &message, &[]).unwrap();
+    let mut messages_received = 0;
+    loop {
+        let mut bytes = [0u8; 256];
+        match syscall::get_message(echo_channel, &mut bytes, &mut []) {
+            Ok((bytes, _handles)) => {
+                info!("Echo sent message back: {:x?}", bytes);
+                let received_message: TestMessage = ptah::from_wire(bytes).unwrap();
+                info!("Message from echo: {:?}", received_message);
+                messages_received += 1;
+
+                // NOTE: this can be changed to receive a total number of messages overall
+                if messages_received == 1 {
+                    break;
+                }
+            }
+            Err(syscall::GetMessageError::NoMessage) => syscall::yield_to_kernel(),
+            Err(err) => panic!("Error getting message from echo: {:?}", err),
+        }
+    }
+
+    let framebuffer = make_framebuffer();
+    framebuffer.clear(Bgr32::pixel(0xaa, 0xaa, 0xaa, 0xff));
+    framebuffer.draw_rect(100, 100, 300, 450, Bgr32::pixel(0xcc, 0x00, 0x00, 0xff));
+    framebuffer.draw_string(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        200,
+        400,
+        Bgr32::pixel(0x00, 0x00, 0x00, 0xff),
+    );
+
+    loop {
+        info!("Yielding from FB");
+        syscall::yield_to_kernel();
+    }
+}
 
 fn make_framebuffer() -> Framebuffer<Bgr32> {
     let (framebuffer_handle, framebuffer_info) = {
@@ -40,61 +113,6 @@ fn make_framebuffer() -> Framebuffer<Bgr32> {
         width: framebuffer_info.width as usize,
         height: framebuffer_info.height as usize,
         stride: framebuffer_info.stride as usize,
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn start() -> ! {
-    syscall::early_log("Hello from FB").unwrap();
-    // Initialise the heap
-    const HEAP_START: usize = 0x600000000;
-    const HEAP_SIZE: usize = 0x4000;
-    let heap_memory_object = syscall::create_memory_object(HEAP_START, HEAP_SIZE, true, false).unwrap();
-    syscall::map_memory_object(heap_memory_object, libpebble::ZERO_HANDLE, 0x0 as *mut usize).unwrap();
-    unsafe {
-        ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE);
-    }
-
-    log::set_logger(&EarlyLogger).unwrap();
-    log::set_max_level(log::LevelFilter::Trace);
-    info!("Simple framebuffer driver is running!");
-
-    /*
-     * Test out the service stuff. We want the echo service.
-     */
-    let echo_channel = syscall::subscribe_to_service("echo.echo").expect("Failed to subscribe to echo service :(");
-    syscall::send_message(echo_channel, &[0xff, 0x00, 0xff, 0xaa], &[]).unwrap();
-    syscall::send_message(echo_channel, &[0x69, 0x69, 0x69], &[]).unwrap();
-    let mut messages_received = 0;
-    loop {
-        let mut bytes = [0u8; 256];
-        match syscall::get_message(echo_channel, &mut bytes, &mut []) {
-            Ok((bytes, _handles)) => {
-                info!("Echo sent message back: {:x?}", bytes);
-                messages_received += 1;
-
-                if messages_received == 2 {
-                    break;
-                }
-            }
-            Err(syscall::GetMessageError::NoMessage) => syscall::yield_to_kernel(),
-            Err(err) => panic!("Error getting message from echo: {:?}", err),
-        }
-    }
-
-    let framebuffer = make_framebuffer();
-    framebuffer.clear(Bgr32::pixel(0xaa, 0xaa, 0xaa, 0xff));
-    framebuffer.draw_rect(100, 100, 300, 450, Bgr32::pixel(0xcc, 0x00, 0x00, 0xff));
-    framebuffer.draw_string(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        200,
-        400,
-        Bgr32::pixel(0x00, 0x00, 0x00, 0xff),
-    );
-
-    loop {
-        info!("Yielding from FB");
-        syscall::yield_to_kernel();
     }
 }
 
