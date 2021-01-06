56e50ac6a09abe578c67ffe0e98dddcf1b751009
Isaac Woods <isaacwoods.home@gmail.com>
2021-01-06 20:45:12 +0000

    gpt: add rest of code before removing library
    Irritatingly, someone fixed all the problems we couldn't be bothered to in
    the `gpt` crate, so it's now much better to use, and we didn't realise. I
    think it's best to switch to using that, but we'll keep our code around in
    the history in case we want to pull it back out at any point.

--- lib/gpt/src/header.rs
+++ lib/gpt/src/header.rs
@@ -13,6 +13,8 @@ pub struct GptHeader {
     last_usable_lba: u64,
     disk_guid: Uuid,
     partition_entry_lba: u64,
+    /// The number of partition entries in the array of this header. Note that this is the number of **entries**,
+    /// not the number of **partitions**, and must have a minimum value of `128`.
     number_of_partition_entries: u32,
     size_of_partition_entry: u32,
     partition_entry_array_crc32: u32,
@@ -38,7 +40,32 @@ impl GptHeader {
             // TODO: we should probably be using v1, but it's harder to call so we're not for now.
             disk_guid: Uuid::new_v4(),
             partition_entry_lba: 2,
-            number_of_partition_entries: 0,
+            number_of_partition_entries: 128,
+            size_of_partition_entry: 128,
+            partition_entry_array_crc32: 0,
+        }
+    }
+
+    pub fn new_backup(last_lba: u64) -> GptHeader {
+        GptHeader {
+            signature: *b"EFI PART",
+            revision: 0x0001_0000,
+            header_size: 92,
+            header_crc32: 0,
+            reserved: 0,
+            my_lba: last_lba,
+            alternate_lba: 1,
+            // This is the first possible LBA we can use for a block size of 512 (1 block for the MBR, 1 for the
+            // primary header, and then 32 for the partition entry array).
+            first_usable_lba: 34,
+            // The last block that can be used for data. This takes the last LBA (which contains the secondary
+            // header), and reserves 32 blocks for the secondary partition entry array, and then takes the next one
+            // (the first free one).
+            last_usable_lba: last_lba - 32 - 1,
+            // TODO: we should probably be using v1, but it's harder to call so we're not for now.
+            disk_guid: Uuid::new_v4(),
+            partition_entry_lba: last_lba - 32,
+            number_of_partition_entries: 128,
             size_of_partition_entry: 128,
             partition_entry_array_crc32: 0,
         }
@@ -77,6 +104,7 @@ impl GptPartitionEntry {
         type_guid: Uuid,
         starting_lba: u64,
         ending_lba: u64,
+        // TODO: use a wrapper type here to make this nicer (bitflags?)
         attributes: u64,
         name: &str,
     ) -> GptPartitionEntry {
@@ -97,8 +125,6 @@ impl GptPartitionEntry {
         }
     }
 
-    // XXX: each partition entry needs to be padded to 128 bytes, so the caller will need to seek to the correct
-    // position for each one
     pub fn write<W: Write>(self, writer: &mut W) -> Result<()> {
         writer.write_all(self.type_guid.as_bytes())?;
         writer.write_all(self.unique_partition_guid.as_bytes())?;
