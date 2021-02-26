e370e101c2a038ba31dac76c7cb8650e0c03b958
Isaac Woods <isaacwoods.home@gmail.com>
2021-02-26 00:58:45 +0000

    kernel_x86_64: remove mutex on serial port
    Constructing it for each message shouldn't be a big deal, as it's not doing
    any initialization, just making the data structure. This will need revision
    when we move to SMP, but the mutex probably wasn't the right solution for
    that either.

--- kernel/kernel_x86_64/src/logger.rs
+++ kernel/kernel_x86_64/src/logger.rs
@@ -1,11 +1,5 @@
 use hal_x86_64::hw::serial::SerialPort;
 use log::{Log, Metadata, Record};
-use spin::Mutex;
-
-/// The COM1 serial port, accessed through the UART 16550 controller found in many old platforms,
-/// and emulated by most emulators. There is no need to initialise it in the kernel, as efiloader already does that
-/// for us.
-static COM1: Mutex<SerialPort> = Mutex::new(unsafe { SerialPort::new(hal_x86_64::hw::serial::COM1) });
 
 /// This handles calls to the log macros throughout the kernel, and writes logging to the COM1
 /// serial port.
@@ -20,7 +14,8 @@ impl Log for KernelLogger {
         use core::fmt::Write;
 
         if self.enabled(record.metadata()) {
-            COM1.lock()
+            let mut serial_port = unsafe { SerialPort::new(hal_x86_64::hw::serial::COM1) };
+            serial_port
                 .write_fmt(format_args!("[{}][{}] {}\n", record.level(), record.target(), record.args()))
                 .unwrap();
         }
