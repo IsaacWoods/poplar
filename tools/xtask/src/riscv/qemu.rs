7c92376fb604b6692c8e4b6d9206c09f2984e354
Isaac Woods <isaacwoods.home@gmail.com>
2022-06-20 16:33:22 +0100

    xtask: support open_display option in RISCV qemu

--- tools/xtask/src/riscv/qemu.rs
+++ tools/xtask/src/riscv/qemu.rs
@@ -1,18 +1,28 @@
 use eyre::{eyre, Result, WrapErr};
-use std::{path::PathBuf, process::Command, string::ToString};
+use std::{path::PathBuf, process::Command};
 
-pub struct RunQemu {
+pub struct RunQemuRiscV {
     pub kernel: PathBuf,
     pub opensbi: PathBuf,
+
+    pub open_display: bool,
 }
 
-impl RunQemu {
-    pub fn new(kernel: PathBuf) -> RunQemu {
-        RunQemu { kernel, opensbi: PathBuf::from("lib/opensbi/build/platform/generic/firmware/fw_jump.elf") }
+impl RunQemuRiscV {
+    pub fn new(kernel: PathBuf) -> RunQemuRiscV {
+        RunQemuRiscV {
+            kernel,
+            opensbi: PathBuf::from("lib/opensbi/build/platform/generic/firmware/fw_jump.elf"),
+            open_display: false,
+        }
     }
 
     pub fn opensbi(self, opensbi: PathBuf) -> Self {
         Self { opensbi, ..self }
+    }
+
+    pub fn open_display(self, open_display: bool) -> Self {
+        Self { open_display, ..self }
     }
 
     pub fn run(self) -> Result<()> {
@@ -21,6 +31,16 @@ impl RunQemu {
         qemu.args(&["-M", "virt"]);
         qemu.args(&["-bios", self.opensbi.to_str().unwrap()]);
         qemu.args(&["-kernel", self.kernel.to_str().unwrap()]);
+
+        // Emit serial on both stdio and to a file
+        qemu.args(&["-chardev", "stdio,id=char0,logfile=qemu_serial_riscv.log"]);
+        qemu.args(&["-serial", "chardev:char0"]);
+
+        if !self.open_display {
+            qemu.args(&["-display", "none"]);
+            // If we're not opening a display, allow connections to the monitor over TCP (open with `nc 127.0.0.1 55555`)
+            qemu.args(&["-monitor", "tcp:127.0.0.1:55555,server,nowait"]);
+        }
 
         println!("QEMU command: {:?}", qemu);
         qemu.status()
