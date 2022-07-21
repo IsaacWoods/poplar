6292e50475d3f314927d3201fbfdd19c612ff5f2
Isaac Woods <isaacwoods.home@gmail.com>
2022-07-21 02:10:23 +0100

    kernel_riscv: pass HART ID and FDT address to `kmain`

--- kernel/kernel_riscv/src/main.rs
+++ kernel/kernel_riscv/src/main.rs
@@ -1,9 +1,12 @@
 #![no_std]
 #![no_main]
+#![feature(pointer_is_aligned)]
 
 /*
  * This is the entry-point jumped to from OpenSBI. It needs to be at the very start of the ELF, so we put it in its
- * own section and then place it manually during linking.
+ * own section and then place it manually during linking. On entry, `a0` contains the current HART's ID, and `a1`
+ * contains the address of the FDT - these match up with the ABI so we can pass these straight as parameters to
+ * `kmain`.
  */
 core::arch::global_asm!(
     "
@@ -13,26 +16,26 @@ core::arch::global_asm!(
         la sp, _stack_top
         mv fp, sp
 
-        li a0, '!'
-        li a6, 0
-        li a7, 1
-        ecall
-        li a0, '\n'
-        ecall
-
         j kmain
 "
 );
 
 #[no_mangle]
-pub fn kmain() -> ! {
+pub fn kmain(hart_id: usize, fdt: *const ()) -> ! {
+    assert!(fdt.is_aligned_to(8));
+
     let uart = unsafe { &mut *(0x10000000 as *mut hal_riscv::hw::uart16550::Uart16550) };
     use core::fmt::Write;
-    write!(uart, "Hello, World!").unwrap();
+    writeln!(uart, "Hello, World!").unwrap();
+    writeln!(uart, "HART ID: {}", hart_id).unwrap();
+    writeln!(uart, "FDT address: {:?}", fdt).unwrap();
     loop {}
 }
 
 #[panic_handler]
 pub fn panic(_info: &core::panic::PanicInfo) -> ! {
+    let uart = unsafe { &mut *(0x10000000 as *mut hal_riscv::hw::uart16550::Uart16550) };
+    use core::fmt::Write;
+    write!(uart, "Panic :(").unwrap();
     loop {}
 }
