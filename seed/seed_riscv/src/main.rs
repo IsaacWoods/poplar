fea7d75d422bd629e0b80d6ed0bf9b877e2dce84
Isaac Woods <isaacwoods.home@gmail.com>
2023-02-12 23:33:43 +0000

    seed_riscv: pull various functionality out of `main`

--- seed/seed_riscv/src/main.rs
+++ seed/seed_riscv/src/main.rs
@@ -95,36 +95,13 @@ pub fn seed_main(hart_id: u64, fdt_ptr: *const u8) -> ! {
 
     let mut kernel_page_table = PageTableImpl::new(MEMORY_MANAGER.allocate(), VAddr::new(0x0));
     let kernel = image::load_kernel(kernel_elf, &mut kernel_page_table, &MEMORY_MANAGER);
-    let mut next_available_address_after_kernel = kernel.next_available_address;
+    let mut next_available_kernel_address = kernel.next_available_address;
 
     /*
-     * Allocate memory for the boot info and start filling it out. We dynamically map the boot info into the
-     * address space after the kernel.
+     * Allocate memory for the boot info and start filling it out.
      */
-    use hal_riscv::kernel_map::PHYSICAL_MAP_BASE;
-    let (boot_info_kernel_address, boot_info) = {
-        let boot_info_physical_start =
-            MEMORY_MANAGER.allocate_n(Size4KiB::frames_needed(mem::size_of::<BootInfo>())).start.start;
-        let identity_boot_info_ptr = usize::from(boot_info_physical_start) as *mut BootInfo;
-        unsafe {
-            ptr::write(identity_boot_info_ptr, BootInfo::default());
-        }
-
-        let boot_info_kernel_address = next_available_address_after_kernel;
-        next_available_address_after_kernel += align_up(mem::size_of::<BootInfo>(), Size4KiB::SIZE);
-
-        kernel_page_table
-            .map_area(
-                boot_info_kernel_address,
-                boot_info_physical_start,
-                align_up(mem::size_of::<BootInfo>(), Size4KiB::SIZE),
-                Flags::default(),
-                &MEMORY_MANAGER,
-            )
-            .unwrap();
-
-        (boot_info_kernel_address, unsafe { &mut *identity_boot_info_ptr })
-    };
+    let (boot_info_kernel_address, boot_info) =
+        create_boot_info(&mut next_available_kernel_address, &mut kernel_page_table);
     boot_info.magic = seed::boot_info::BOOT_INFO_MAGIC;
     boot_info.fdt_address = Some(PAddr::new(fdt_ptr as usize).unwrap());
 
@@ -133,6 +110,7 @@ pub fn seed_main(hart_id: u64, fdt_ptr: *const u8) -> ! {
      * TODO: we should probably do this properly by walking the FDT (you need RAM + devices) but we currently just
      * map 32GiB.
      */
+    use hal_riscv::kernel_map::PHYSICAL_MAP_BASE;
     kernel_page_table
         .map_area(
             PHYSICAL_MAP_BASE,
@@ -143,24 +121,7 @@ pub fn seed_main(hart_id: u64, fdt_ptr: *const u8) -> ! {
         )
         .unwrap();
 
-    /*
-     * Allocate the kernel heap and dynamically map it into the kernel address space.
-     */
-    const KERNEL_HEAP_SIZE: hal::memory::Bytes = hal::memory::kibibytes(800);
-    boot_info.heap_address = next_available_address_after_kernel;
-    boot_info.heap_size = KERNEL_HEAP_SIZE;
-    next_available_address_after_kernel += KERNEL_HEAP_SIZE;
-    let kernel_heap_physical_start =
-        MEMORY_MANAGER.allocate_n(Size4KiB::frames_needed(KERNEL_HEAP_SIZE)).start.start;
-    kernel_page_table
-        .map_area(
-            boot_info.heap_address,
-            kernel_heap_physical_start,
-            KERNEL_HEAP_SIZE,
-            Flags { writable: true, ..Default::default() },
-            &MEMORY_MANAGER,
-        )
-        .unwrap();
+    alloc_and_map_kernel_heap(&mut next_available_kernel_address, &mut kernel_page_table, boot_info);
 
     /*
      * Identity-map all of Seed into the kernel's page tables, so we don't page fault when switching to them.
@@ -215,4 +176,58 @@ pub fn seed_main(hart_id: u64, fdt_ptr: *const u8) -> ! {
             options(nostack, noreturn)
         )
     }
+}
+
+/// Allocate memory for the boot info, and dynamically map it into the address space after the kernel.
+fn create_boot_info<'a>(
+    next_available_kernel_address: &mut VAddr,
+    kernel_page_table: &mut PageTableImpl,
+) -> (VAddr, &'a mut BootInfo) {
+    let boot_info_physical_start =
+        MEMORY_MANAGER.allocate_n(Size4KiB::frames_needed(mem::size_of::<BootInfo>())).start.start;
+    let identity_boot_info_ptr = usize::from(boot_info_physical_start) as *mut BootInfo;
+    unsafe {
+        ptr::write(identity_boot_info_ptr, BootInfo::default());
+    }
+
+    let boot_info_kernel_address = *next_available_kernel_address;
+    *next_available_kernel_address += align_up(mem::size_of::<BootInfo>(), Size4KiB::SIZE);
+
+    kernel_page_table
+        .map_area(
+            boot_info_kernel_address,
+            boot_info_physical_start,
+            align_up(mem::size_of::<BootInfo>(), Size4KiB::SIZE),
+            Flags::default(),
+            &MEMORY_MANAGER,
+        )
+        .unwrap();
+
+    (boot_info_kernel_address, unsafe { &mut *identity_boot_info_ptr })
+}
+
+/// Allocate memory for the kernel heap, and dynamically map it into the address space after the kernel. We tell
+/// the kernel where to find it via the boot info.
+fn alloc_and_map_kernel_heap(
+    next_available_kernel_address: &mut VAddr,
+    kernel_page_table: &mut PageTableImpl,
+    boot_info: &mut BootInfo,
+) {
+    const KERNEL_HEAP_SIZE: hal::memory::Bytes = hal::memory::kibibytes(800);
+
+    boot_info.heap_address = *next_available_kernel_address;
+    boot_info.heap_size = KERNEL_HEAP_SIZE;
+    *next_available_kernel_address += KERNEL_HEAP_SIZE;
+
+    let kernel_heap_physical_start =
+        MEMORY_MANAGER.allocate_n(Size4KiB::frames_needed(KERNEL_HEAP_SIZE)).start.start;
+    kernel_page_table
+        .map_area(
+            boot_info.heap_address,
+            kernel_heap_physical_start,
+            KERNEL_HEAP_SIZE,
+            Flags { writable: true, ..Default::default() },
+            &MEMORY_MANAGER,
+        )
+        .unwrap();
 }
