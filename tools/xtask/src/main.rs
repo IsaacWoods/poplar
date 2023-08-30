00efc5d0cd68d00e8b7adc4f7c07a504d547e94e
Isaac Woods <isaacwoods.home@gmail.com>
2023-08-30 18:20:44 +0100

    Remove in-tree versions of `uefi-rs` and `ucs2-rs`
    These are no longer needed, and it's always nice to have less stuff in-tree.

--- tools/xtask/src/main.rs
+++ tools/xtask/src/main.rs
@@ -65,8 +65,6 @@ fn main() -> Result<()> {
             clean(PathBuf::from("lib/pci_types"))?;
             clean(PathBuf::from("lib/poplar_util"))?;
             clean(PathBuf::from("lib/ptah"))?;
-            clean(PathBuf::from("lib/ucs2-rs"))?;
-            clean(PathBuf::from("lib/uefi-rs"))?;
             Ok(())
         }
     }
