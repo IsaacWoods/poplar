e2d55d387cc1a8c3940cb8c51fd9b63316520d4e
Isaac Woods <isaacwoods.home@gmail.com>
2023-09-25 22:24:05 +0100

    `std`: re-export correct items from `alloc` and `core`
    Real `std` re-exports various things from `core` and `alloc`, so we should
    do the same. Also include items from `alloc` in the prelude.

--- lib/std/src/lib.rs
+++ lib/std/src/lib.rs
@@ -1,8 +1,40 @@
 #![allow(internal_features)]
-#![feature(lang_items, prelude_import)]
+#![feature(lang_items, prelude_import, async_iterator, core_intrinsics)]
 #![no_std]
 
+extern crate alloc;
+
 use core::panic::PanicInfo;
+
+/*
+ * Public re-exports. Most of this is copied from real `std`, plus our `poplar` library.
+ * NOTE: deprecated re-exports, such as `std::i32` (and friends), are not included.
+ */
+pub use alloc::{borrow, boxed, fmt, format, rc, slice, str, string, vec};
+pub use core::{
+    any,
+    array,
+    async_iter,
+    cell,
+    char,
+    clone,
+    cmp,
+    convert,
+    default,
+    future,
+    hash,
+    hint,
+    intrinsics,
+    iter,
+    marker,
+    mem,
+    ops,
+    option,
+    pin,
+    ptr,
+    result,
+};
+pub use poplar;
 
 // Import our own prelude for this crate
 #[prelude_import]
@@ -14,9 +46,21 @@ pub use prelude::rust_2021::*;
  */
 pub mod prelude {
     pub mod rust_2018 {
+        pub use alloc::{
+            boxed::Box,
+            format,
+            string::{String, ToString},
+            vec::Vec,
+        };
         pub use core::{assert_eq, panic, prelude::rust_2018::*};
     }
     pub mod rust_2021 {
+        pub use alloc::{
+            boxed::Box,
+            format,
+            string::{String, ToString},
+            vec::Vec,
+        };
         pub use core::{assert_eq, panic, prelude::rust_2021::*};
     }
 }
