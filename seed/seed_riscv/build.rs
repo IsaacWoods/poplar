d108a4cf30a0eb95c4de013ec4f3d03a362137fe
Isaac Woods <isaacwoods.home@gmail.com>
2023-11-04 00:41:48 +0000

    `seed_riscv`: use different linker scripts on the two platforms
    We need to load at different addresses on each platform - the easiest way
    to do this seems to be duplicating the linker scripts, but this doesn't
    seem super optimal. Fine for now.

--- seed/seed_riscv/build.rs
+++ seed/seed_riscv/build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("cargo:rerun-if-changed=link.ld");
+    // TODO: wonder if we can do this based on a platform feature?
+    println!("cargo:rerun-if-changed=rv64_virt.ld");
+    println!("cargo:rerun-if-changed=mq_pro.ld");
 }
