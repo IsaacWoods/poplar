47479621d4b9e5b29d911fb7969d1a2938bbee60
Isaac Woods <isaacwoods.home@gmail.com>
2023-11-28 16:19:30 +0000

    `lib/std`: provide correct linker script for arch + RV64 linker script
    I'm still not 100% on how the GP-relative addressing works in the ideal case,
    so this might not be 100% correct, but at least does link Rust programs...
    
    We'll see once we can load + run stuff in userspace.

--- lib/std/build.rs
+++ lib/std/build.rs
@@ -1,12 +1,21 @@
 use std::{env, fs::File, io::Write, path::PathBuf};
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
     // Put the linker script somewhere the linker can find it. We can then specify the linker script with just
     // `link-arg=-Tlink.ld`.
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    File::create(out.join("link.ld")).unwrap().write_all(include_bytes!("link.ld")).unwrap();
+    match env::var_os("TARGET").unwrap().to_str() {
+        Some("x86_64-poplar") => {
+            println!("cargo:rerun-if-changed=x64.x");
+            File::create(out.join("link.ld")).unwrap().write_all(include_bytes!("x64.ld")).unwrap();
+        }
+        Some("riscv64gc-unknown-none-elf") => {
+            println!("cargo:rerun-if-changed=rv64.x");
+            File::create(out.join("link.ld")).unwrap().write_all(include_bytes!("rv64.ld")).unwrap();
+        }
+        _ => panic!("Building Poplar `std` for unsupported target!"),
+    }
     println!("cargo:rustc-link-search={}", out.display());
-
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=link.x");
 }
