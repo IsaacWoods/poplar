975be9ac68f82ecc95731ee1fcc2be1f29c9aeb1
IsaacWoods <isaacwoods.home@gmail.com>
2023-12-07 02:05:26 +0000

    Deploying to gh-pages from  @ 3325119f2f148aa738fcf3af485b66861ba9b33e 🚀

--- doc/kernel/sidebar-items.js
+++ doc/kernel/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["create_framebuffer","load_task"],"mod":["heap_allocator","memory","object","pci","scheduler","syscall"],"static":["ALLOCATOR","FRAMEBUFFER","PCI_ACCESS","PCI_INFO","PHYSICAL_MEMORY_MANAGER"],"trait":["Platform"]};
+window.SIDEBAR_ITEMS = {"fn":["create_framebuffer","initialize_pci","load_task"],"mod":["heap_allocator","memory","object","pci","scheduler","syscall"],"static":["ALLOCATOR","FRAMEBUFFER","PCI_ACCESS","PCI_INFO","PHYSICAL_MEMORY_MANAGER"],"trait":["Platform"]};
