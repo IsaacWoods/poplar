3aeea6e8c1d9555b12d53c15dce3fa2dfae5d1ab
Isaac Woods <isaacwoods.home@gmail.com>
2024-05-03 12:08:04 +0100

    ginkgo: extremely basic test REPL

--- ginkgo/src/main.rs
+++ ginkgo/src/main.rs
@@ -1,4 +1,21 @@
-fn main() {
+use ginkgo::lex::Lex;
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
     // TODO: repl
     println!("Hello, world!");
+
+    loop {
+        print!("> ");
+        io::stdout().flush()?;
+
+        // Get a line of input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        let lex = Lex::new(&input);
+        for token in lex {
+            println!("Token: {:?}", token);
+        }
+    }
 }
