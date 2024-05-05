dd1514bd54689cff940de815871bf06c4c9c48dd
Isaac Woods <isaacwoods.home@gmail.com>
2024-05-05 23:49:57 +0100

    ginkgo: allow running a script from file before entering REPL

--- ginkgo/src/main.rs
+++ ginkgo/src/main.rs
@@ -1,8 +1,25 @@
 use ginkgo::{interpreter::Interpreter, parse::Parser};
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    path::Path,
+};
 
 fn main() -> io::Result<()> {
     let mut interpreter = Interpreter::new();
+
+    // If we were passed a path, load and run that file
+    if std::env::args().count() > 1 {
+        let path = std::env::args().nth(1).unwrap();
+        println!("Executing script at {}", path);
+
+        let source = std::fs::read_to_string(Path::new(&path)).unwrap();
+        let parser = Parser::new(&source);
+        let output = parser.parse().unwrap();
+
+        for statement in output {
+            interpreter.eval_stmt(statement);
+        }
+    }
 
     loop {
         print!("> ");
